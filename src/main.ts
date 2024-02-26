740b277c1cd03f625b285a611b6395f222dbf858
argon1025 <argon1025@gmail.com>
2024-02-27 01:00:33 +0900

    feat: Swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('SchoolNewsFeed API')
+    .setDescription('학교 소식을 전달하고 받아보는 뉴스피드 기능 예제')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, { customSiteTitle: 'SchoolNewsFeed API' });
+
   await app.listen(3000);
 }
 bootstrap();

0730dbf4d328d99afec6ba02accbae13a5f39de0
Seongrok.lee <55491354+argon1025@users.noreply.github.com>
2024-02-27 01:10:02 +0900

    #1 프로젝트 기본 구성 (#16)
    * chore: 노드 버전 매니저 설정
    
    * feat: esLint Airbnb 코드 스타일 적용
    
    * feat: husky pre-push 구성
    
    * feat: Swagger 추가
    
    * feat: 환경변수 파일, configService 추가, 패키지 스크립트 대응

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('SchoolNewsFeed API')
+    .setDescription('학교 소식을 전달하고 받아보는 뉴스피드 기능 예제')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, { customSiteTitle: 'SchoolNewsFeed API' });
+
+  // Port 설정
+  const servicePort = configService.getOrThrow<number>('SERVICE_PORT');
+
+  await app.listen(servicePort);
+  Logger.log(`Server is running on: ${await app.getUrl()}, Swagger: ${await app.getUrl()}/api`, 'Bootstrap');
 }
 bootstrap();

e61885262ee6aaeb66d58b4ec61e164c42fc9056
argon1025 <argon1025@gmail.com>
2024-02-27 01:04:49 +0900

    feat: 환경변수 파일, configService 추가, 패키지 스크립트 대응

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
 
   // Swagger 설정
   const config = new DocumentBuilder()
@@ -14,6 +17,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, { customSiteTitle: 'SchoolNewsFeed API' });
 
-  await app.listen(3000);
+  // Port 설정
+  const servicePort = configService.getOrThrow<number>('SERVICE_PORT');
+
+  await app.listen(servicePort);
+  Logger.log(`Server is running on: ${await app.getUrl()}, Swagger: ${await app.getUrl()}/api`, 'Bootstrap');
 }
 bootstrap();

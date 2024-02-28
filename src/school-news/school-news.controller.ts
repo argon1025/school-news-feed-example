7b3036369f4a2cb29ecb33f77bea3d5529d8baf1
Seongrok.lee <55491354+argon1025@users.noreply.github.com>
2024-02-29 01:25:21 +0900

    #8 소식 삭제 기능 (#25)
    * feat: 소식 삭제 기능
    
    * feat: 소식 삭제 API
    
    * feat: 소식 삭제 기능 테스트 케이스 추가

--- src/school-news/school-news.controller.ts
+++ src/school-news/school-news.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Inject, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Inject, Param, Patch, Post } from '@nestjs/common';
 import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { plainToInstance } from 'class-transformer';
 import { SCHOOL_NEWS_SERVICE, SchoolNewsServiceBase } from './type/school-news.service.interface';
 import { AddErrorObjectToSwagger, ERROR } from '../common/exception/all-exception/error.constant';
 import { CreateSchoolNewsRequest, CreateSchoolNewsResponse } from './dto/create-school-news.dto';
 import { PatchSchoolNewsRequest, PatchSchoolNewsResponse } from './dto/patch-school-news.dto';
+import { DeleteSchoolNewsRequest, DeleteSchoolNewsResponse } from './dto/delete-school-news.dto';
 
 @Controller('school')
 @ApiTags('학교-소식')
@@ -13,6 +14,24 @@ export class SchoolNewsController {
     @Inject(SCHOOL_NEWS_SERVICE)
     private readonly schoolNewsService: SchoolNewsServiceBase,
   ) {}
+
+  @Delete('news/:schoolNewsId')
+  @ApiOperation({
+    summary: '학교 소식 삭제',
+    description: '학교 소식을 삭제합니다. <br>해당 학교 멤버 중 선생님 Role을 가진 사용자만 사용할 수 있습니다.',
+  })
+  @ApiParam({ name: 'schoolNewsId', description: '학교 소식 ID', example: '7f2023de-6c27-4e01-bb14-30c975315f6a' })
+  @AddErrorObjectToSwagger([
+    ERROR.INTERNAL_SERVER_ERROR,
+    ERROR.SCHOOL_NEWS_NOT_FOUND,
+    ERROR.SCHOOL_MEMBER_NOT_FOUND,
+    ERROR.SCHOOL_PERMISSION_CHECK,
+    ERROR.USER_NOT_FOUND,
+  ])
+  async delete(@Param('schoolNewsId') schoolNewsId: string, @Body() request: DeleteSchoolNewsRequest) {
+    const result = await this.schoolNewsService.delete({ ...request, id: schoolNewsId });
+    return plainToInstance(DeleteSchoolNewsResponse, { schoolNewsId: result.id });
+  }
 
   @Patch('news/:schoolNewsId')
   @ApiOperation({
--- src/school-news/school-news.service.ts
+++ src/school-news/school-news.service.ts
@@ -1,10 +1,13 @@
 import { Injectable, Inject, NotFoundException, Logger, ForbiddenException } from '@nestjs/common';
 import { SchoolUserRole } from '@prisma/client';
+import { DateTime } from 'luxon';
 import { PrismaRepository } from '../common/prisma/prisma.repository';
 import { USER_SERVICE, UserServiceBase } from '../user/type/user.service.interface';
 import {
   CreateOptions,
   CreateResult,
+  DeleteOptions,
+  DeleteResult,
   SchoolNewsServiceBase,
   UpdateOptions,
   UpdateResult,
@@ -100,6 +103,47 @@ export class SchoolNewsService implements SchoolNewsServiceBase {
       return { id: result.id };
     } catch (error) {
       Logger.error('학교 소식 수정 실패', error, 'SchoolNewsService.update');
+      throw new NotFoundException(ERROR.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  /** 소식 삭제 */
+  async delete(options: DeleteOptions): Promise<DeleteResult> {
+    const { id, userId } = options;
+
+    // 유저 정보가 유효한지 확인
+    await this.userService.findOne({ id: userId });
+
+    // 소식이 유효한지 확인
+    const schoolNews = await this.prismaRepository.schoolNews.findUnique({ where: { id, deletedAt: null } });
+    if (!schoolNews) {
+      throw new NotFoundException(ERROR.SCHOOL_NEWS_NOT_FOUND);
+    }
+
+    // 멤버 정보가 유효한지 확인
+    const schoolMember = await this.prismaRepository.schoolMember.findFirst({
+      where: { userId, schoolId: schoolNews.schoolId, deletedAt: null },
+    });
+    // 학교에 속해 있는 멤버인지 확인
+    if (!schoolMember) {
+      throw new NotFoundException(ERROR.SCHOOL_MEMBER_NOT_FOUND);
+    }
+    // 소식을 수정할 권한이 있는지 확인
+    if (schoolMember.role !== SchoolUserRole.TEACHER) {
+      throw new ForbiddenException(ERROR.SCHOOL_PERMISSION_CHECK);
+    }
+
+    // 소식 삭제
+    try {
+      const result = await this.prismaRepository.schoolNews.update({
+        where: { id },
+        data: {
+          deletedAt: DateTime.utc().toJSDate(),
+        },
+      });
+      return { id: result.id };
+    } catch (error) {
+      Logger.error('학교 소식 삭제 실패', error, 'SchoolNewsService.delete');
       throw new NotFoundException(ERROR.INTERNAL_SERVER_ERROR);
     }
   }
